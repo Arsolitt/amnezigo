a01f74ea841ad341b07407de8b3ca56797815350
Arsolitt <arsolitt@gmail.com>
2026-03-20 18:00:15 +0300

    refactor: update obfuscation types for server/client separation

--- internal/config/types.go
+++ internal/config/types.go
@@ -2,37 +2,49 @@ package config
 
 import "time"
 
+type HeaderRange struct {
+	Min, Max uint32
+}
+
 type ServerConfig struct {
 	Interface   InterfaceConfig
 	Peers       []PeerConfig
-	Obfuscation ObfuscationConfig
+	Obfuscation ServerObfuscationConfig
 }
 
 type InterfaceConfig struct {
-	PrivateKey string
-	PublicKey  string
-	Address    string
-	ListenPort int
-	MTU        int
-	PostUp     string
-	PostDown   string
-	MainIface  string
-	TunName    string
+	PrivateKey     string
+	PublicKey      string
+	Address        string
+	ListenPort     int
+	MTU            int
+	PostUp         string
+	PostDown       string
+	MainIface      string
+	TunName        string
+	EndpointV4     string
+	EndpointV6     string
+	ClientToClient bool
 }
 
 type PeerConfig struct {
-	Name         string
-	PrivateKey   string
-	PublicKey    string
-	PresharedKey string
-	AllowedIPs   string
-	CreatedAt    time.Time
+	Name              string
+	PrivateKey        string
+	PublicKey         string
+	PresharedKey      string
+	AllowedIPs        string
+	CreatedAt         time.Time
+	ClientObfuscation *ClientObfuscationConfig
 }
 
-type ObfuscationConfig struct {
-	Jc, Jmin, Jmax     int
-	S1, S2, S3, S4     int
-	H1, H2, H3, H4     uint32
+type ServerObfuscationConfig struct {
+	Jc, Jmin, Jmax int
+	S1, S2, S3, S4 int
+	H1, H2, H3, H4 HeaderRange
+}
+
+type ClientObfuscationConfig struct {
+	ServerObfuscationConfig
 	I1, I2, I3, I4, I5 string
 }
 
@@ -46,7 +58,7 @@ type ClientInterfaceConfig struct {
 	Address     string
 	DNS         string
 	MTU         int
-	Obfuscation ObfuscationConfig
+	Obfuscation ClientObfuscationConfig
 }
 
 type ClientPeerConfig struct {
